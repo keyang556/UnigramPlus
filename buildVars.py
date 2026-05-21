6ed8bf522d8f3e7a64c05f3df9e342d4148c6439
Keyang556 <65295310+keyang556@users.noreply.github.com>
2026-05-21 13:02:20 +0800

    5.5.0
    2026.1

--- buildVars.py
+++ buildVars.py
@@ -1,52 +1,49 @@
-# -*- coding: UTF-8 -*-
-
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-
-# Since some strings in `addon_info` are translatable,
-# we need to include them in the .po files.
-# Gettext recognizes only strings given as parameters to the `_` function.
-# To avoid initializing translations in this module we simply roll our own "fake" `_` function
-# which returns whatever is given to it as an argument.
-def _(arg):
-	return arg
+from site_scons.site_tools.NVDATool.typings import AddonInfo, BrailleTables, SymbolDictionaries
+from site_scons.site_tools.NVDATool.utils import _
 
 
 # Add-on information variables
-addon_info = {
+addon_info = AddonInfo(
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "unigramPlus",
-	# Add-on summary, usually the user visible name of the addon.
-	# Translators: Summary for this add-on
-	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("UnigramPlus"),
+	addon_name="unigramPlus",
+	# Add-on summary/title, usually the user visible name of the add-on
+	# Translators: Summary/title for this add-on
+	# to be shown on installation and add-on information found in add-on store.
+	addon_summary=_("UnigramPlus"),
 	# Add-on description
-	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description": _("""This add-on adds a lot of hotkeys for quick navigation through the main elements of the Unigram application, convenient interaction with Chat messages, and also adds many small improvements that make working in Unigram much easier"""),
+	# Translators: Long description to be shown for this add-on on add-on information from add-on store.
+	addon_description=_(
+		"""This add-on adds a lot of hotkeys for quick navigation through the main elements of the Unigram application, convenient interaction with Chat messages, and also adds many small improvements that make working in Unigram much easier""",
+	),
 	# version
-	"addon_version": "5.2.0",
+	addon_version="5.5.0",
+	# Brief changelog for this version
+	# Translators: what's new content for the add-on version to be shown in the add-on store.
+	addon_changelog=_("Compatibility update for NVDA 2026.1."),
 	# Author(s)
-	"addon_author": "Kostya Gladkiy<gladkiy.kostya@gmail.com>",
+	addon_author="Kostya Gladkiy<gladkiy.kostya@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/Kostya-Gladkiy/UnigramPlus",
+	addon_url="https://github.com/Kostya-Gladkiy/UnigramPlus",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": "https://github.com/Kostya-Gladkiy/UnigramPlus",
+	addon_sourceURL="https://github.com/Kostya-Gladkiy/UnigramPlus",
 	# Documentation file name
-	"addon_docFileName": "readme.html",
-	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2021.2.0",
-	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.1.0",
+	addon_docFileName="readme.html",
+	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
+	addon_minimumNVDAVersion="2021.2.0",
+	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
+	addon_lastTestedNVDAVersion="2026.1.0",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
+	addon_updateChannel=None,
 	# Add-on license such as GPL 2
-	"addon_license": "GPL 2",
-	# URL for the license document the ad-on is licensed under
-	"addon_licenseURL": "https://www.gnu.org/licenses/gpl-2.0.html",
-}
+	addon_license="GPL 2",
+	# URL for the license document the add-on is licensed under
+	addon_licenseURL="https://www.gnu.org/licenses/gpl-2.0.html",
+)
 
 # Define the python files that are the sources of your add-on.
 # You can either list every file (using ""/") as a path separator,
@@ -56,23 +53,34 @@ addon_info = {
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/appModules/*.py", "addon/GlobalPlugins/UnigramPlus/*.py"]
+pythonSources: list[str] = ["addon/appModules/*.py", "addon/GlobalPlugins/UnigramPlus/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py"]
+i18nSources: list[str] = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
-excludedFiles = []
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+excludedFiles: list[str] = []
 
 # Base language for the NVDA add-on
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
-baseLanguage = "en"
+# You must also edit .gitignore file to specify base language files to be ignored.
+baseLanguage: str = "en"
 
 # Markdown extensions for add-on documentation
 # Most add-ons do not require additional Markdown extensions.
 # If you need to add support for markup such as tables, fill out the below list.
 # Extensions string must be of the form "markdown.extensions.extensionName"
 # e.g. "markdown.extensions.tables" to add tables.
-markdownExtensions = []
+markdownExtensions: list[str] = []
+
+# Custom braille translation tables
+# If your add-on includes custom braille tables (most will not), fill out this dictionary.
+brailleTables: BrailleTables = {}
+
+# Custom speech symbol dictionaries
+# If your add-on includes custom speech symbol dictionaries (most will not), fill out this dictionary.
+symbolDictionaries: SymbolDictionaries = {}
